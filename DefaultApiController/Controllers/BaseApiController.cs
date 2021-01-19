96fe90699d6a75447342cd406f50e50bf32b42be
APlatonenkov <platonenkov87@gmail.com>
2021-01-19 08:28:41 +0300

    change controller

--- DefaultApiController/Controllers/BaseApiController.cs
+++ DefaultApiController/Controllers/BaseApiController.cs
@@ -3,14 +3,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using DefaultApiClientServiceController.Entity;
 using DefaultApiClientServiceController.Interface;
+using Microsoft.AspNet.OData;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DefaultApiClientServiceController.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Produces("application/json")]
-    public abstract class BaseApiController<T, T2,T3> : ControllerBase where T : IBaseDataService<T2,T3> where T2 : BaseEntity<T3>
+    //[Route("api/[controller]")]
+    //[ApiController]
+    //[Produces("application/json")]
+    public abstract class BaseApiController<T, T2,T3> : ODataController where T : IBaseDataService<T2,T3> where T2 : BaseEntity<T3>
     {
         protected readonly T Service;
 
@@ -26,6 +27,7 @@ namespace DefaultApiClientServiceController.Controllers
         }
         // GET: api/[controller]/All
         [HttpGet]
+        [EnableQuery]
         public virtual async Task<ActionResult<List<T2>>> GetAll()
         {
             var result = await Service.GetAllAsync();
@@ -34,6 +36,7 @@ namespace DefaultApiClientServiceController.Controllers
 
         // GET: api/[controller]/5
         [HttpGet("{id}")]
+        [EnableQuery]
         public virtual async Task<ActionResult<T2>> Get(T3 id)
         {
             var data = await Service.GetAsync(id);
@@ -48,6 +51,7 @@ namespace DefaultApiClientServiceController.Controllers
 
         // PUT: api/[controller]/5
         [HttpPut("{id}")]
+        [EnableQuery]
         public virtual async Task<ActionResult<T2>> Put(T3 id, T2 data)
         {
             if (!id.Equals(data.ID))
@@ -66,6 +70,7 @@ namespace DefaultApiClientServiceController.Controllers
 
         // POST: api/[controller]/
         [HttpPost]
+        [EnableQuery]
         public virtual async Task<ActionResult<T2>> Post(T2 data)
         {
             var entity = await Service.AddNewAsync(data);
@@ -75,6 +80,7 @@ namespace DefaultApiClientServiceController.Controllers
 
         // DELETE: api/[controller]/5
         [HttpDelete("{id}")]
+        [EnableQuery]
         public virtual async Task<ActionResult<T2>> Delete(T3 id)
         {
             var entity = await Service.DeleteAsync(id);
@@ -88,6 +94,7 @@ namespace DefaultApiClientServiceController.Controllers
 
         // GET: api/[controller]/Exist/5
         [HttpGet("Exist/{id}")]
+        [EnableQuery]
         public virtual async Task<bool> Exists(T3 id) => await Service.ExistsAsync(id);
         // GET: api/[controller]/SaveChanges
         [HttpGet("SaveChanges")]
