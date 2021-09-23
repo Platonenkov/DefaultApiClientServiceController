746bacc446946477881cc8ecb4f22fbddc4fe34b
APlatonenkov <platonenkov87@gmail.com>
2021-09-23 08:20:23 +0300

    fix swagger conflict

--- Test/ApiServer/Startup.cs
+++ Test/ApiServer/Startup.cs
@@ -17,6 +17,7 @@ using Domain;
 using Infrastucture;
 using Infrastucture.Context;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OData.Edm;
 using Newtonsoft.Json;
@@ -44,6 +45,29 @@ namespace ApiServer
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiServer", Version = "v1" });
+                c.ResolveConflictingActions(apiDescriptions =>
+                {
+                    var descriptions = apiDescriptions as ApiDescription[] ?? apiDescriptions.ToArray();
+                    var first = descriptions.First(); // build relative to the 1st method
+                    var parameters = descriptions.SelectMany(d => d.ParameterDescriptions).ToList();
+
+                    first.ParameterDescriptions.Clear();
+                    // add parameters and make them optional
+                    foreach (var parameter in parameters)
+                        if (first.ParameterDescriptions.All(x => x.Name != parameter.Name))
+                        {
+                            first.ParameterDescriptions.Add(new ApiParameterDescription
+                            {
+                                ModelMetadata = parameter.ModelMetadata,
+                                Name = parameter.Name,
+                                ParameterDescriptor = parameter.ParameterDescriptor,
+                                Source = parameter.Source,
+                                IsRequired = false,
+                                DefaultValue = null
+                            });
+                        }
+                    return first;
+                });
             });
 
 
